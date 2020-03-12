2488d34b7969f3da5a4cc2547a1f1dd8198d7bac
Alex <alexander.tate4@gmail.com>
2020-03-11 20:10:44 -0500

    asdasd

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,6 @@
 $('.test').click(function(e){
-    $('svg').removeAttr('viewBox');
-    $('svg').each(function () { $(this)[0].setAttribute('viewBox', '0 0 200 200'); });
+   /* $('svg').removeAttr('viewBox');
+    $('svg').each(function () { $(this).setAttribute('viewBox', '0 0 200 200'); }); */
+
+    $('svg').attr('viewBox', '0 0 200 200');
 });
