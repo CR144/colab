77230273c82fbf4b22524bb06d4bb03d06b37003
Alex <alexander.tate4@gmail.com>
2020-03-11 20:04:30 -0500

    trying again

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
 $('.test').click(function(){
     $('svg').removeAttr('viewBox');
-    $('svg').each(function () { $(this)[0].setAttribute('viewBox', '0 0 200 200') });
+    $('svg').each(function () { $(this)[0].setAttribute('viewBox', '0 0 200 200'); });
 });
