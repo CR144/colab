c7029f9303788b48d119766f08ac52774949492b
Alex <alexander.tate4@gmail.com>
2020-03-11 19:55:44 -0500

    sodfgndsfgb

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
-$('#test').click(function(){
-    $('.masterPlan').removeAttr('viewBox');
-    $('.masterPlan').each(function (){
+$('.test').click(function(){
+    $('#masterPlan').removeAttr('viewBox');
+    $('#masterPlan').each(function (){
         $(this)[0].setAttribute('viewBox', '0 0 200 200');
     });
 });
