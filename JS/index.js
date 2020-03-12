d08ef6cda92a60523bc77de171ce4ce3367c243b
Alex <alexander.tate4@gmail.com>
2020-03-11 20:01:53 -0500

    sdfsdf

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,5 @@
 $('.test').click(function(){
-    $('#masterPlan').removeAttr('viewBox');
-    $('#masterPlan').each(function (){
-        $(this)[0].setAttribute('viewBox', '0 0 200 200');
-    });
+    $('svg').attr('viewBox', '0 0 200 200');
+    
+    
 });
