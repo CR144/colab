4e1c50e7ef8b36f6e08ce13a43ee8223976f0b72
Alex <alexander.tate4@gmail.com>
2020-03-11 20:05:27 -0500

    trying

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-$('.test').click(function(){
+$('.test').click(function(e){
     $('svg').removeAttr('viewBox');
     $('svg').each(function () { $(this)[0].setAttribute('viewBox', '0 0 200 200'); });
 });
