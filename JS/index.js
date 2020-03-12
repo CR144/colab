43f95a1d8cae92e3fb29391a8cb6ff20c89cf0c5
Alex <alexander.tate4@gmail.com>
2020-03-11 20:15:19 -0500

    wdqwd

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
 $('.test').click(function(){
-   document.getElementById('svg').attr('viewBox' , '0,0,200,200');
+   $('#masterPlan').attr('viewBox' , '0,0,200,200');
 });
 });
